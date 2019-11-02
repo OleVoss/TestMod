c2be8c9d02ee51e52e9e913d6673dcb81cfa9638
OleVoss <57292942+OleVoss@users.noreply.github.com>
2019-11-03 00:03:57 +0100

    reset

--- src/main/java/com/bruh/testmod/ModMain.java
+++
@@ -1,8 +0,0 @@
-package com.bruh.testmod;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(ModMain.MOD_ID)
-public class ModMain {
-	public static final String MOD_ID = "testmod";
-}
--- src/main/java/com/bruh/testmod/lists/BlockList.java
+++
@@ -1,7 +0,0 @@
-package com.bruh.testmod.lists;
-
-import net.minecraft.block.Block;
-
-public class BlockList {
-	public static Block TEST_ORE = null;
-}
