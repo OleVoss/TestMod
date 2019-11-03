d4c5b43561edf9711cea50270f6069680cbe54ed
OleVoss <57292942+OleVoss@users.noreply.github.com>
2019-11-03 15:10:12 +0100

    enum tool material

--- src/main/java/com/example/examplemod/lists/ModItems.java
+++ src/main/java/com/example/examplemod/lists/ModItems.java
@@ -4,6 +4,12 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 
 public class ModItems {
+	//items
 	public static Item test_item = null;
+	
+	//tools
+	public static Item test_sword = null;
+	
+	// block items
 	public static Item test_ore = null;
 }
